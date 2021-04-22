a4bb1ebd7623ce2380579094e38d97b398c90a4b
bzettek <59636807+bzettek@users.noreply.github.com>
2021-04-22 17:58:56 -0400

    Update server.py

--- server.py
+++ server.py
@@ -16,7 +16,7 @@ app.add_api("master.yaml")
 # create a URL route in our application for "/"
 @app.route("/")
 def home():
-    msg = {"msg": "It's working!"}
+    msg = {"ENGR-E 222 Final Project\n By: Brandon Zettek"}
     return jsonify(msg)
 
 
